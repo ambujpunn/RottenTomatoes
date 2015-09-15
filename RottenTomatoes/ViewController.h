7f57cd950322099aa9abf6df7d9fdd3474519e32
Ambuj Punn <apunn@diddribble-lm.corp.yahoo.com>
2015-09-15 15:40:09 -0700

    Initial Commit

---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Ambuj Punn on 9/15/15.
+//  Copyright (c) 2015 Ambuj Punn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RottenTomatoes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RottenTomatoes
+//
+//  Created by Ambuj Punn on 9/15/15.
+//  Copyright (c) 2015 Ambuj Punn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
